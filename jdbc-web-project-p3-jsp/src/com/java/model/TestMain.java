62b2872cbbc34a41c7d1dd50e218c2c15c12d72f
Ashok <Ashok@LAPTOP-HQIVR8EM>
2019-05-26 00:09:28 +0530

    change A

---
+++ jdbc-web-project-p3-jsp/src/com/java/model/TestMain.java
@@ -0,0 +1,16 @@
+package com.java.model;
+
+/*
+ * ashok
+ * 
+ * Change A
+ */
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test Conflict");
+
+	}
+
+}
